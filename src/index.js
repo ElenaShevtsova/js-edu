bc5e1e2fb0e8951b3439205ef03b0c66c83bee60
Elena Shevtsova <helena_shevts@mail.ru>
2019-10-01 19:41:29 +0300

    init: start js-edu

--- src/index.js
+++ src/index.js
@@ -5,10 +5,47 @@
  * @returns number of weeks needed for finish education
  */
 module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+  focus,
+  knowsProgramming,
+  config
+) {
+  if (focus == 'family' && knowsProgramming == true) {
+    return Math.ceil(800 / config.family);
+  }
+  if (focus == 'friends' && knowsProgramming == true) {
+    return Math.ceil(800 / config.friends);
+  }
+  if (focus == 'normal_life' && knowsProgramming == true) {
+    return Math.ceil(800 / config.normal_life);
+  }
+  if (focus == 'profession' && knowsProgramming == true) {
+    return Math.ceil(800 / config.profession);
+  }
+  if (focus == 'carrier' && knowsProgramming == true) {
+    return Math.ceil(800 / config.carrier);
+  }
+  if (focus == 'top_peformance' && knowsProgramming == true) {
+    return Math.ceil(800 / config.top_peformance);
+  }
+  if (focus == 'family' && knowsProgramming == false) {
+    return Math.ceil(1300 / config.family);
+  }
+  if (focus == 'family' && knowsProgramming == false) {
+    return Math.ceil(1300 / config.family);
+  }
+  if (focus == 'friends' && knowsProgramming == false) {
+    return Math.ceil(1300 / config.friends);
+  }
+  if (focus == 'normal_life' && knowsProgramming == false) {
+    return Math.ceil(1300 / config.normal_life);
+  }
+  if (focus == 'profession' && knowsProgramming == false) {
+    return Math.ceil(1300 / config.profession);
+  }
+  if (focus == 'carrier' && knowsProgramming == false) {
+    return Math.ceil(1300 / config.carrier);
+  }
+  if (focus == 'top_peformance' && knowsProgramming == false) {
+    return Math.ceil(1300 / config.top_peformance);
+  }
+};
